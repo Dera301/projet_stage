d76a512185046fb8ece3a9fc99e28a79b884b629
Dera301 <hajaaridera@gmail.com>
2025-10-11 09:52:35 +0300

    infinityfree backend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,16 +1,10 @@
 <?php
-// Headers CORS complets
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: https://nytranoko.netlify.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin, Accept");
-
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin, Accept");
 header("Access-Control-Allow-Credentials: true");
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+header("Content-Type: application/json; charset=UTF-8");
 
-// Répondre immédiatement aux requêtes OPTIONS
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     http_response_code(200);
     exit();
